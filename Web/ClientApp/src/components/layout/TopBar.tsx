b483e9ff08a8d68d54e570ae43bc029f32e1daa1
sam nesbitt <Sam.Nesbitt@devfacto.com>
2019-03-19 22:16:20 -0600

    update app layout

--- Web/ClientApp/src/components/layout/TopBar.tsx
+++ Web/ClientApp/src/components/layout/TopBar.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import { AppBar, Toolbar, Typography } from '@material-ui/core';
 
 export function TopBar() {
-    return <AppBar position="static" color="default">
+    return <AppBar
+        position="static"
+        color="primary"
+    >
         <Toolbar>
             <Typography variant="h6" color="inherit">
                 Net Worth Calculator
